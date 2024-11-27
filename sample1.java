8253638ff9aadaf4c95930320d08b54a1237216f
rutujapachpute2434 <rutujapachpute2872003@gmail.com>
2024-11-27 10:51:03 +0530

    Update sample1.java

--- sample1.java
+++ sample1.java
@@ -1,9 +1,8 @@
 class fact{
   public static void main (String args[]){
-    scanner sc=new scanner(System.in);
+   
     int fact=1;
-    System.out.println("Enter the number:");
-    int no=sc.nextInt();
+    int no=2;
     for(int i=1;i<=no;i++){
       fact=fact*i;
     }
