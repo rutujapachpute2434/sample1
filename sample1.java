59022d47f4494fe0f18a58acd59806e8a281ba10
rutujapachpute2434 <rutujapachpute2872003@gmail.com>
2024-11-27 10:39:04 +0530

    Create sample1.java

---
+++ sample1.java
@@ -0,0 +1,11 @@
+class fact{
+  public static void main (String args[]){
+    int fact=1;
+    System.out.println("Enter the number:");
+    int no=sc.nextInt();
+    for(int i=1;i<=number;i++){
+      fact=fact*i;
+    }
+    System.out.println("factorial of "+no+"is" +fact);
+  }
+}
